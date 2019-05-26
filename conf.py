69a43bbacb6210fe409b69a7fc0c03996570b0da
samprager <sprager@usc.edu>
2019-05-26 12:25:56 -0700

    formatting

--- conf.py
+++ conf.py
@@ -17,8 +17,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Documentation with JupyterLab'
-copyright = '2019, Samuel Prager'
+project = 'Creating Documentation with JupyterLab'
+copyright = '2019, Samuel Prager, University of Southern California'
 author = 'Samuel Prager'
 
 # The full version, including alpha/beta/rc tags
